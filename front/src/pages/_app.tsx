87763304268fac9b9fd72d904c5b2c2f64a1c596
Tim Rybicki <t.rybicki@gmail.com>
2021-02-07 16:58:51 -0500

    Add favicon

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,10 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react';
-
 import theme from '../theme';
 import { AppProps } from 'next/app';
 
-// The Component prop is the active page, so whenever you navigate between routes, Component will
-// change to the new page. Therefore, any props you send to Component will be received by the page.
+// The Component prop is the active page
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme} resetCSS>
