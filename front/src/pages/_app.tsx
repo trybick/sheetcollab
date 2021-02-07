a16ade3abfb0a9251b122522028f366dd198895b
Tim Rybicki <t.rybicki@gmail.com>
2021-02-07 16:03:14 -0500

    Remove extra stuff

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,14 +1,16 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
 
-import theme from '../theme'
-import { AppProps } from 'next/app'
+import theme from '../theme';
+import { AppProps } from 'next/app';
 
+// The Component prop is the active page, so whenever you navigate between routes, Component will
+// change to the new page. Therefore, any props you send to Component will be received by the page.
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
