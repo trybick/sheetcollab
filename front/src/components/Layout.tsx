b23171d55a8776aa924e223856d1ad05e1bb7193
Tim Rybicki <t.rybicki@gmail.com>
2021-03-06 09:19:46 -0500

    Add html tab title

--- front/src/components/Layout.tsx
+++ front/src/components/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/Head';
 import { useRouter } from 'next/router';
 import Header from './Header';
 
@@ -7,6 +8,11 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <>
+      <Head>
+        <title>Sheet Collab</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
       {shouldShowHeader && <Header />}
       {children}
     </>
