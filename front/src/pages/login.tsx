afde784788ce9569de6a713c151fd49be456beae
Tim Rybicki <t.rybicki@gmail.com>
2021-03-06 16:21:27 -0500

    Separate login form in login component

--- front/src/pages/login.tsx
+++ front/src/pages/login.tsx
@@ -1,76 +1,7 @@
-import {
-  Box,
-  Button,
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Input,
-} from '@chakra-ui/react';
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { emailRegex } from '../utils/regex';
-
-type FormData = {
-  email: string;
-  password: string;
-};
-
-const formSchema = {
-  email: {
-    required: { value: true, message: 'Email is required' },
-    pattern: { value: emailRegex, message: 'Please enter a valid email' },
-  },
-  password: {
-    required: 'Password is required',
-  },
-};
+import LoginForm from 'src/components/login/LoginForm';
 
 const Login = () => {
-  const { handleSubmit, errors, register } = useForm<FormData>();
-
-  const onSubmit = handleSubmit(({ email, password }) => {
-    console.log('email:', email);
-    console.log('password:', password);
-  });
-
-  return (
-    <Flex background="gray.50" justify="center" minH="100vh">
-      <Box background="white" boxShadow="md" borderRadius="4px" m="auto" p={10} w="400px">
-        <Box as="form" onSubmit={onSubmit}>
-          <FormControl isInvalid={Boolean(errors.email)}>
-            <FormLabel htmlFor="email">Email</FormLabel>
-            <Input
-              id="email"
-              name="email"
-              placeholder="Enter email"
-              ref={register(formSchema.email)}
-              autoFocus
-            />
-            <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
-          </FormControl>
-
-          <FormControl isInvalid={Boolean(errors.password)} mt={4}>
-            <FormLabel>Password</FormLabel>
-            <Input
-              id="password"
-              name="password"
-              placeholder="Enter password"
-              ref={register(formSchema.password)}
-              type="password"
-            />
-            <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
-          </FormControl>
-
-          <Box textAlign="right">
-            <Button isLoading={false} mt="28px" type="submit" colorScheme="gray" w="100%">
-              Login
-            </Button>
-          </Box>
-        </Box>
-      </Box>
-    </Flex>
-  );
+  return <LoginForm />;
 };
 
 export default Login;
