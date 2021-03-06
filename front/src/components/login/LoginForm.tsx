86aa49d09ea12d8f7a504391e8f17a4ac6bfa644
Tim Rybicki <t.rybicki@gmail.com>
2021-03-06 16:37:22 -0500

    Add Sign up button above login form

--- front/src/components/login/LoginForm.tsx
+++ front/src/components/login/LoginForm.tsx
@@ -34,41 +34,39 @@ const LoginForm = () => {
   });
 
   return (
-    <Flex background="gray.50" justify="center" minH="100vh">
-      <Box background="white" boxShadow="md" borderRadius="4px" m="auto" p={10} w="400px">
-        <Box as="form" onSubmit={onSubmit}>
-          <FormControl isInvalid={Boolean(errors.email)}>
-            <FormLabel htmlFor="email">Email</FormLabel>
-            <Input
-              id="email"
-              name="email"
-              placeholder="Enter email"
-              ref={register(formSchema.email)}
-              autoFocus
-            />
-            <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
-          </FormControl>
+    <Box background="white" boxShadow="md" borderRadius="4px" m="2rem auto 0" p={10} w="400px">
+      <Box as="form" onSubmit={onSubmit}>
+        <FormControl isInvalid={Boolean(errors.email)}>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <Input
+            id="email"
+            name="email"
+            placeholder="Enter email"
+            ref={register(formSchema.email)}
+            autoFocus
+          />
+          <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+        </FormControl>
 
-          <FormControl isInvalid={Boolean(errors.password)} mt={4}>
-            <FormLabel>Password</FormLabel>
-            <Input
-              id="password"
-              name="password"
-              placeholder="Enter password"
-              ref={register(formSchema.password)}
-              type="password"
-            />
-            <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
-          </FormControl>
+        <FormControl isInvalid={Boolean(errors.password)} mt={4}>
+          <FormLabel>Password</FormLabel>
+          <Input
+            id="password"
+            name="password"
+            placeholder="Enter password"
+            ref={register(formSchema.password)}
+            type="password"
+          />
+          <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+        </FormControl>
 
-          <Box textAlign="right">
-            <Button isLoading={false} mt="28px" type="submit" colorScheme="gray" w="100%">
-              Login
-            </Button>
-          </Box>
+        <Box textAlign="right">
+          <Button isLoading={false} mt="28px" type="submit" colorScheme="gray" w="100%">
+            Login
+          </Button>
         </Box>
       </Box>
-    </Flex>
+    </Box>
   );
 };
 
--- front/src/pages/login.tsx
+++ front/src/pages/login.tsx
@@ -1,7 +1,25 @@
+import { Box, Flex, Heading, Link, Text } from '@chakra-ui/react';
 import LoginForm from 'src/components/login/LoginForm';
 
 const Login = () => {
-  return <LoginForm />;
+  return (
+    <Flex background="gray.50" direction="column" minH="100vh">
+      <Box m="7rem auto 0">
+        <Heading as="h2" textAlign="center">
+          Sign in to your account
+        </Heading>
+
+        <Text fontWeight="medium" mt={4} textAlign="center">
+          Don't have an account?
+          <Link color="blue.600" ml={1}>
+            Sign up
+          </Link>
+        </Text>
+      </Box>
+
+      <LoginForm />
+    </Flex>
+  );
 };
 
 export default Login;
