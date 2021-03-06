947ace78c75fa0b51fa425b4cb9df34649eb7dbe
Tim Rybicki <t.rybicki@gmail.com>
2021-03-05 19:27:39 -0500

    Create basic navbar with Login

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,5 +1,27 @@
-import { Box } from '@chakra-ui/react';
+import { Button, Flex, HStack } from '@chakra-ui/react';
 
-const Index = () => <Box>Hello world</Box>;
+const Index = () => (
+  <Flex background="white" boxShadow="md" px={8} py={4}>
+    <Flex align="center" mr="20px">
+      Sheet Collab
+    </Flex>
+
+    <HStack spacing="10px">
+      <Button colorScheme="gray" variant="ghost">
+        My Sheets
+      </Button>
+      <Button colorScheme="gray" variant="ghost">
+        Add Sheet
+      </Button>
+      <Button colorScheme="gray" variant="ghost">
+        Search
+      </Button>
+    </HStack>
+
+    <Flex ml="auto">
+      <Button>Log in</Button>
+    </Flex>
+  </Flex>
+);
 
 export default Index;
