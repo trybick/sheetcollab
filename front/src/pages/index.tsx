2fe7f1e944bd49397d5c6a4e18d4212db174561d
Tim Rybicki <t.rybicki@gmail.com>
2021-03-07 15:21:28 -0500

    Remove unused query on index

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,23 +1,6 @@
-import { useQuery, gql } from '@apollo/client';
 import { Flex } from '@chakra-ui/react';
 
-const SHEET_BY_ID = gql`
-  query sheetById($id: Int!) {
-    sheet(id: $id) {
-      title
-      artist
-      id
-      year
-    }
-  }
-`;
-
 const Index = () => {
-  const { loading, error, data } = useQuery(SHEET_BY_ID, { variables: { id: 1 } });
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
   return <Flex>Home page</Flex>;
 };
 
