8cfa77a63fee9e8d87924c7b626e2f0f75c139bb
Tim Rybicki <t.rybicki@gmail.com>
2021-03-09 17:23:01 -0500

    Create table for recently added

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,11 +1,38 @@
 import { Flex } from '@chakra-ui/react';
+import { Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
 import { useRecentSheetsQuery } from '../generated/graphql';
+import { parseISO } from 'date-fns';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
 const Index = () => {
-  const { data, loading, error } = useRecentSheetsQuery();
-  console.log('data:', data);
+  const { data, loading } = useRecentSheetsQuery();
 
-  return <Flex>Home page</Flex>;
+  const createTableRows = () => {
+    return data?.recentSheets.map(({ artist, id, createdAt, title }) => (
+      <Tr key={id}>
+        <Td>{title}</Td>
+        <Td>{artist}</Td>
+        <Td>{formatDistanceToNow(parseISO(createdAt))}</Td>
+      </Tr>
+    ));
+  };
+
+  return (
+    !loading && (
+      <Flex maxW="500px" m="200px auto">
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Song</Th>
+              <Th>Artist</Th>
+              <Th>Date</Th>
+            </Tr>
+          </Thead>
+          <Tbody>{createTableRows()}</Tbody>
+        </Table>
+      </Flex>
+    )
+  );
 };
 
 export default Index;
