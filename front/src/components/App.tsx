1a8f578cd2383659faac20352eb1c719df928d75
Tim Rybicki <t.rybicki@gmail.com>
2021-03-13 09:03:19 -0500

    Separate routes

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -3,18 +3,22 @@ import Layout from 'src/components/Layout';
 import Homepage from 'src/components/Homepage';
 import LoginPage from 'src/components/login/LoginPage';
 
+const routes = (
+  <>
+    <Route path="/" exact>
+      <Homepage />
+    </Route>
+    <Route path="/login">
+      <LoginPage />
+    </Route>
+  </>
+);
+
 function App() {
   return (
     <Router>
       <Layout>
-        <Switch>
-          <Route path="/" exact>
-            <Homepage />
-          </Route>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-        </Switch>
+        <Switch>{routes}</Switch>
       </Layout>
     </Router>
   );
