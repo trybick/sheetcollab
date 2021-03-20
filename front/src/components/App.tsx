f20d4e66f1ad8f54848391b021701d9681dc7cd9
Tim Rybicki <t.rybicki@gmail.com>
2021-03-20 16:59:56 -0400

    Cleanup atom and App structure

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -2,14 +2,12 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { mappedRoutes } from 'helpers/routes/routeMap';
 import Layout from 'components/core/Layout';
 
-function App() {
-  return (
-    <Router>
-      <Layout>
-        <Switch>{mappedRoutes}</Switch>
-      </Layout>
-    </Router>
-  );
-}
+const App = () => (
+  <Router>
+    <Layout>
+      <Switch>{mappedRoutes}</Switch>
+    </Layout>
+  </Router>
+);
 
 export default App;
