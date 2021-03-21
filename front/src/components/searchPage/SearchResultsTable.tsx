efbbdfaf1f674970c6eac62e823159d8a255b238
Tim Rybicki <t.rybicki@gmail.com>
2021-03-21 15:42:53 -0400

    Add no results message to table

--- front/src/components/searchPage/SearchResultsTable.tsx
+++ front/src/components/searchPage/SearchResultsTable.tsx
@@ -2,21 +2,35 @@ import { Badge, Box, Flex, Heading, Table, Tbody, Td, Th, Thead, Tr } from '@cha
 import { parseISO } from 'date-fns';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import { FilterSheetsQuery } from 'graphql/generated/hooks';
+import NoResultsMessage from './NoResultsMessage';
 
 const SearchResultsTable = ({
   results,
 }: {
   results: FilterSheetsQuery['filterSheets'] | undefined;
 }) => {
-  const createTableRows = () =>
-    results?.map(({ artist, id, createdAt, title, users }) => (
-      <Tr key={id}>
-        <Td>{title}</Td>
-        <Td>{artist}</Td>
-        <Td>{users[users.length - 1].username}</Td>
-        <Td>{formatDistanceToNow(parseISO(createdAt), { addSuffix: true })}</Td>
-      </Tr>
-    ));
+  const createTable = () => (
+    <Table m="30px auto 0" maxW="800px" variant="simple">
+      <Thead>
+        <Tr>
+          <Th>Song</Th>
+          <Th>Artist</Th>
+          <Th>Added By</Th>
+          <Th>Added</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {results?.map(({ artist, id, createdAt, title, users }) => (
+          <Tr key={id}>
+            <Td>{title}</Td>
+            <Td>{artist}</Td>
+            <Td>{users[users.length - 1].username}</Td>
+            <Td>{formatDistanceToNow(parseISO(createdAt), { addSuffix: true })}</Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  );
 
   return (
     <Box m="25px auto 0" maxW="800px">
@@ -24,21 +38,10 @@ const SearchResultsTable = ({
         <Heading as="h3" fontSize="24px">
           Search
         </Heading>
-
         <Badge>{results?.length} results</Badge>
       </Flex>
 
-      <Table m="30px auto 0" maxW="800px" variant="simple">
-        <Thead>
-          <Tr>
-            <Th>Song</Th>
-            <Th>Artist</Th>
-            <Th>Added By</Th>
-            <Th>Added</Th>
-          </Tr>
-        </Thead>
-        <Tbody>{createTableRows()}</Tbody>
-      </Table>
+      {results?.length ? createTable() : <NoResultsMessage />}
     </Box>
   );
 };
