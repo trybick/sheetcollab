4c23c2526381f7d4d84ecdf1959ac13b044d95eb
Tim Rybicki <t.rybicki@gmail.com>
2021-04-17 10:54:19 -0400

    Add Add New button to profile page

--- front/src/components/MyProfilePage/MyProfilePage.tsx
+++ front/src/components/MyProfilePage/MyProfilePage.tsx
@@ -1,8 +1,10 @@
-import { Box, Flex, Heading, Spinner } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+import { Box, Button, Flex, Heading, Spinner } from '@chakra-ui/react';
 import { Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
 import { useMySheetsQuery } from 'graphql/generated/hooks';
 import { parseISO } from 'date-fns';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import { ROUTES } from 'helpers/routes/routeMap';
 
 const MyProfilePage = () => {
   const { data, loading } = useMySheetsQuery({ fetchPolicy: 'cache-and-network' });
@@ -23,24 +25,38 @@ const MyProfilePage = () => {
       <Spinner size="xl" />
     </Flex>
   ) : (
-    <Box maxW="500px" m="200px auto">
-      <Heading as="h3" fontSize="22px" mb="12px" textAlign="center">
-        My Sheets
-      </Heading>
+    <>
+      <Box m="40px 80px" textAlign="right">
+        <Button
+          as={RouterLink}
+          colorScheme="blue"
+          size="sm"
+          textAlign="right"
+          to={ROUTES.ADD_SHEET}
+        >
+          Add New
+        </Button>
+      </Box>
 
-      <Table size="sm" variant="simple">
-        <Thead>
-          <Tr>
-            <Th>Song</Th>
-            <Th>Artist</Th>
-            <Th>Added</Th>
-            <Th>Year</Th>
-            <Th>Notes</Th>
-          </Tr>
-        </Thead>
-        <Tbody>{createTableRows()}</Tbody>
-      </Table>
-    </Box>
+      <Box maxW="500px" m="40px auto">
+        <Heading as="h3" fontSize="22px" mb="12px" textAlign="center">
+          My Sheets
+        </Heading>
+
+        <Table size="sm" variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Song</Th>
+              <Th>Artist</Th>
+              <Th>Added</Th>
+              <Th>Year</Th>
+              <Th>Notes</Th>
+            </Tr>
+          </Thead>
+          <Tbody>{createTableRows()}</Tbody>
+        </Table>
+      </Box>
+    </>
   );
 };
 
