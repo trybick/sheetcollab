235da231f513f6a13b87cf8f9bd9d4cbff4172de
Tim Rybicki <t.rybicki@gmail.com>
2021-04-25 11:09:41 -0400

    Create shell for popular arists section

--- front/src/components/HomePage/HomePage.tsx
+++ front/src/components/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
 import { Flex } from '@chakra-ui/react';
+import PopularArists from './PopularArtists';
 import RecentlyAddedSheets from './RecentlyAddedSheets';
 
 const Homepage = () => {
   return (
-    <Flex>
+    <Flex flexDir="column">
+      <PopularArists />
       <RecentlyAddedSheets />
     </Flex>
   );
---
+++ front/src/components/HomePage/PopularArtists.tsx
@@ -0,0 +1,15 @@
+import { Box, Heading } from '@chakra-ui/react';
+
+const PopularArists = () => {
+  return (
+    <Box maxW="500px" m="40px auto">
+      <Heading as="h3" fontSize="17px" mb="6px">
+        Popular artists
+      </Heading>
+
+      <Box>tba</Box>
+    </Box>
+  );
+};
+
+export default PopularArists;
