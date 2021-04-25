592f1d6d25ffc48c3977f92e8aee60e3b81885c7
Tim Rybicki <t.rybicki@gmail.com>
2021-04-25 10:48:22 -0400

    Make homepage recently added a table

--- front/src/components/HomePage/RecentlyAddedSheets.tsx
+++ front/src/components/HomePage/RecentlyAddedSheets.tsx
@@ -1,30 +1,44 @@
-import { Box, Heading, Link } from '@chakra-ui/react';
+import { Box, Button, Heading, Table, Tbody, Td, Tr } from '@chakra-ui/react';
 import { useRecentSheetsQuery } from 'graphql/generated/hooks';
 import { useHasWaitedForInitialLoad } from 'helpers/hooks/useHasWaitedForInitialLoad';
 import SkeletonStack from 'components/common/SkeletonStack';
+import { parseISO } from 'date-fns';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
 const RecentlyAddedSheets = () => {
   const { data, loading } = useRecentSheetsQuery();
   const { hasWaited } = useHasWaitedForInitialLoad();
 
-  const createItems = () =>
-    data?.recentSheets.map(({ artist, id, title }) => (
-      <Box key={id}>
-        <Link color="blue.500">{artist}</Link> - <Link color="blue.500">{title}</Link>
-      </Box>
+  const createRows = () =>
+    data?.recentSheets.map(({ artist, createdAt, id, title }) => (
+      <Tr key={id}>
+        <Td>
+          <Button color="blue.500" variant="link">
+            {title}
+          </Button>
+        </Td>
+        <Td>
+          <Button color="blue.500" variant="link">
+            {artist}
+          </Button>
+        </Td>
+        <Td>{formatDistanceToNow(parseISO(createdAt), { addSuffix: true })}</Td>
+      </Tr>
     ));
 
   return (
     <Box maxW="500px" m="200px auto">
-      <Heading as="h3" fontSize="18px" mb="6px" textAlign="center">
-        Recently Added
+      <Heading as="h3" fontSize="18px" mb="6px">
+        Recently added
       </Heading>
 
       <Box>
         {loading && hasWaited ? (
           <SkeletonStack height="20px" numRows={8} mb="4px" />
         ) : (
-          createItems()
+          <Table size="sm">
+            <Tbody>{createRows()}</Tbody>
+          </Table>
         )}
       </Box>
     </Box>
